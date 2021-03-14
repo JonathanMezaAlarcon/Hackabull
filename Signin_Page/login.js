5974a77f7d87d0f11776275e12a7ae0307e200c1
Emily Lin <44381949+emilyylin@users.noreply.github.com>
2021-03-14 06:42:09 -0400

    styling signup page + fixing buttons

--- Signin_Page/login.js
+++ Signin_Page/login.js
@@ -1,58 +1,58 @@
-// var mysql = require('mysql');
-// var express = require('express');
-// var session = require('express-session');
-// var bodyParser = require('body-parser');
-// var path = require('path');
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-// var connection = mysql.createConnection({
-// 	host     : '127.0.0.1',
-// 	user     : 'root',
-// 	password : 'Password12345',
-// 	database : 'nodelogin'
-// });
+var connection = mysql.createConnection({
+	host     : '127.0.0.1',
+	user     : 'root',
+	password : 'Password12345',
+	database : 'nodelogin'
+});
 
-// var app = express ();
+var app = express ();
 
-// app.use(session({
-// 	secret: 'secret',
-// 	resave: true,
-// 	saveUninitialized: true
-// }));
-// app.use(bodyParser.urlencoded({extended : true}));
-// app.use(bodyParser.json());
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
-// app.get('/', function(request, response) {
-// 	response.sendFile(path.join(__dirname + '/signin.html'));
-// });
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/signin.html'));
+});
 
-// app.post('/auth', function(request, response) {
-// 	var username = request.body.username;
-// 	var password = request.body.password;
-// 	if (username && password) {
-// 		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-// 			if (results.length > 0) {
-// 				request.session.loggedin = true;
-// 				request.session.username = username;
-// 				response.redirect('/home');
-// 			} else {
-// 				response.send('Incorrect Username and/or Password!');
-// 			}			
-// 			response.end();
-// 		});
-// 	} else {
-// 		response.send('Please enter Username and Password!');
-// 		response.end();
-// 	}
-// });
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
 
-// app.get('/home', function(request, response) {
-// 	if (request.session.loggedin) {
-// 		response.send('Welcome back, ' + request.session.username + '!');
-// 	} else {
-// 		response.send('Please login to view this page!');
-// 	}
-// 	response.end();
-// });
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
 
-// app.listen(3000);
+app.listen(3000);
 
